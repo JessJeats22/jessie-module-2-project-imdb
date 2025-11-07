5a3a6015ebb2b1c893772c86807e7e9ff8805f98
JessieJ <jvwiltshire22@gmail.com>
2025-11-07 13:13:21 +0000

    added api.js file and installed netlify packages

---
+++ netlify/functions/api.js
@@ -0,0 +1,77 @@
+// * Importing Dependencies
+import serverless from 'serverless-http'
+import express from 'express'
+import morgan from 'morgan'
+import methodOverride from 'method-override'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv/config'
+import path from 'path';
+import session from 'express-session'
+import MongoStore from 'connect-mongo'
+
+
+// * Importing Controllers / Routers
+import authRouter from '../../controllers/auth.js'
+import movieRouter from '../../controllers/movies.js'
+// import reviewRoute from './controllers/reviews.js'
+
+
+// * Importing Middleware
+import isSignedIn from '../../middleware/is-signed-in.js';
+import passUserToView from '../../middleware/pass-user-to-view'
+
+const app = express()
+
+app.set('view engine', 'ejs');
+
+
+// * Calling Session Middleware
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI })
+    })
+);
+
+
+// * Calling Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(express.static('public'));
+app.use(passUserToView);
+app.use(methodOverride('_method'));
+
+
+
+
+// * Calling controllers/Routers - intructing Express app to use certain controllers based on URL pattern
+app.use("/auth", authRouter); 
+app.use('/movies', isSignedIn, movieRouter);
+
+
+
+// * ROUTES 
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    user: req.session.user,
+  });
+});
+
+
+// * Connections
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI)
+    console.log('🔒 Database connection established')
+  } catch (error) {
+    console.error(error)
+  }
+}
+connect()
+
+
+// * Server
+export const handler = serverless(app)
